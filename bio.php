2cff5f088efb8b02edff00bbeac2e290ae3bbf65
Natalie Schoch <natalie@zenpayroll.com>
2018-11-21 13:15:21 -0800

    added separate about scss

--- bio.php
+++ bio.php
@@ -4,12 +4,12 @@
   <section class='intro'>
     <div class='container'>
       <div class='row'>
+        <div class='col-sm-4 reveal load-hidden'>
+          <img src='images/img-natalie-schoch.png', class='img-responsive', alt='Natalie Schoch, designer', title='Natalie Schoch, designer'/>
+        </div>
         <div class='col-sm-8 reveal load-hidden'>
           <h1>
-            Hi, I'm Natalie. 👋 I'm a Bay Area native living in San Francisco, CA. I love to work across all graphic mediums but have been playing with code since the days of Myspace and Neopets. I can generally be found listening to too much music, drinking cappuccinos, and overwatering my plant family.
-          </h1>
-          <h1>
-            The best way to contact me is via email, but you can also check out my Dribbble, Linkedin, Medium, and Github. (Or Twitter, but it’s mostly retweets).
+            Hi, I'm Natalie. 👋 I'm a Bay Area native living in San Francisco, California. I love to work across all graphic mediums but have been playing with code since the days of Myspace and Neopets. I can generally be found listening to too much music, drinking cappuccinos, and overwatering my plant family.
           </h1>
           <h1>
             <span class='bold'>Interviewed in:</span> <a href='#'>Interface Lovers</a>, and <a href='#'>Proto-typer.io</a>
@@ -17,9 +17,6 @@
           <h1>
             This site was designed and <a href=''>hand-coded</a> using good ol' HTML and SCSS by Natalie Schoch. It uses Basis Grotesque and <a href='https://scrollrevealjs.org/' target='_blank'>Scroll Reveal</a>.
           </h1>
-        </div>
-        <div class='col-sm-4 reveal load-hidden'>
-          <img src='images/img-natalie-schoch.png', class='img-responsive', alt='Natalie Schoch, designer', title='Natalie Schoch, designer'/>
         </div>
       </div>
     </div>
